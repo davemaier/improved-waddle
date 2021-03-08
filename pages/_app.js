833df705480d24e2463f596b12c40880b74243e6
David Maier <david.maier@atempo.at>
2021-03-08 21:13:44 +0100

    watn los hier

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { UserProvider } from "@auth0/nextjs-auth0";
+import { createClient, Provider } from "urql";
+
+const client = createClient({
+  url: "/api/graphql",
+});
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <UserProvider>
+      <Provider value={client}>
+        <Component {...pageProps} />
+      </Provider>
+    </UserProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
